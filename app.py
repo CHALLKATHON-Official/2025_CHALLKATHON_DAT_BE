0fd6f698a0927c497f5d8bf5598518a6e70e3eb1
lee-seohee <leesh021202@naver.com>
2025-06-24 17:45:28 +0900

    change port

--- app.py
+++ app.py
@@ -842,4 +842,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
