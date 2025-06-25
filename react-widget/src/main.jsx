4fde2773c647159a107c209cbb0ed80980bd787f
Honghyu Koh <issackoh@naver.com>
2025-06-25 16:57:09 +0900

    0625 design finish

--- react-widget/src/main.jsx
+++ react-widget/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('react-root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
