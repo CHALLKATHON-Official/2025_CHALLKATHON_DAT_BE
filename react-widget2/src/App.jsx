214db9c549f751068982007fada786cf19bc6c35
Honghyu Koh <issackoh@naver.com>
2025-06-26 13:57:48 +0900

    0626 react 시도

---
+++ react-widget2/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import AwesomeBackground from "./AwesomeBackground";
+
+function App() {
+  return (
+    <>
+      <AwesomeBackground />
+      <div className="main">ㅗㅑ</div>
+    </>
+  );
+}
+
+export default App;
